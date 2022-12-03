cc5508c6b91d0c883efe0deec3622629a2b37ead
Erik Lilja <erik.lilja@prodtex.com>
2022-12-03 20:28:41 +0100

    AoC Dec3

---
+++ Dec3/Part1.cs
@@ -0,0 +1,76 @@
+﻿using System.IO.Pipes;
+
+namespace Dec3;
+
+public class Part1
+{
+    public static void Calculate()
+    {
+        Console.WriteLine("Part1");
+
+        List<string> lines = new List<string>();
+        lines = File.ReadLines("Input.txt").ToList();
+
+        Dictionary<string, int> priorities = new Dictionary<string, int>();
+
+        int i = 1;
+        for (char c='a'; c<='z';c++)
+        {
+            priorities.Add(c.ToString(),i);
+
+            int unicode = (int)c;
+            int upperUnicode = c - 32;
+            char upperChar = (char)upperUnicode;
+            
+            priorities.Add(upperChar.ToString(),i+26);
+            i++;
+        }
+        
+        //Split compartments
+        var compartments = SplitLinesIntoCompartments(lines);
+
+        int result = 0;
+        foreach (var compartment in compartments)
+        {
+            result += GetPriority(compartment, priorities);
+        }
+
+        Console.WriteLine("Result:");
+        Console.WriteLine(result);
+
+
+    }
+
+    private static List<Tuple<string,string>> SplitLinesIntoCompartments(List<string> lines)
+    {
+        List<Tuple<string, string>> compartments = new List<Tuple<string, string>>();
+        foreach (var line in lines)
+        {
+            var first = line.Substring(0, line.Length / 2);
+            var last = line.Substring(line.Length / 2, line.Length / 2);
+            compartments.Add(new Tuple<string, string>(first,last));
+        }
+        
+        return compartments;
+    }
+
+    private static int GetPriority(Tuple<string, string> compartment, Dictionary<string, int> priorities)
+    {
+        var first = compartment.Item1;
+        var last = compartment.Item2;
+
+        var firstList = first.ToArray().ToList();
+        var lastList = last.ToArray().ToList();
+
+        var duplicates = firstList.Intersect(lastList).ToList();
+
+        int result = 0;
+        foreach (var duplicate in duplicates)
+        {
+            result += priorities[duplicate.ToString()];
+        }
+        
+        
+        return result;
+    }
+}
---
+++ Dec3/Part2.cs
@@ -0,0 +1,71 @@
+﻿namespace Dec3;
+
+public class Part2
+{
+    public static void Calculate()
+    {
+        Console.WriteLine("Part2");
+
+        List<string> lines = new List<string>();
+        lines = File.ReadLines("Input.txt").ToList();
+
+        Dictionary<string, int> priorities = new Dictionary<string, int>();
+
+        int i = 1;
+        for (char c='a'; c<='z';c++)
+        {
+            priorities.Add(c.ToString(),i);
+
+            int unicode = (int)c;
+            int upperUnicode = c - 32;
+            char upperChar = (char)upperUnicode;
+            
+            priorities.Add(upperChar.ToString(),i+26);
+            i++;
+        }
+
+        int result = 0;
+        for (int j = 0; j < lines.Count; j += 3)
+        {
+            var badge = GetBadge(lines[j], lines[j + 1], lines[j + 2]);
+            var prority = GetPriority(badge,priorities);
+            result += prority;
+        }
+        
+        
+        
+        
+        Console.WriteLine("Result:");
+        Console.WriteLine(result);
+
+
+    }
+
+
+
+    private static int GetPriority(string badge, Dictionary<string,int> priorities)
+    {
+        int result;
+
+        result = priorities[badge];
+        
+        
+        return result;
+    }
+
+    private static string GetBadge(string bag1, string bag2, string bag3)
+    {
+        string result;
+
+        var bag1List = bag1.ToArray().ToString();
+        var bag2List = bag2.ToArray().ToString();
+        var bag3List = bag3.ToArray().ToString();
+
+        var common = bag1.Intersect(bag2).ToList();
+        common = common.Intersect(bag3).ToList();
+
+        result = common[0].ToString();
+
+        return result;
+    }
+}
