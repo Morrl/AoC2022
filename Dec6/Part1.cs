6cc577e4e895d4e22d212b36c4da10b098e82fcc
Erik Lilja <erik.lilja@prodtex.com>
2022-12-06 08:05:18 +0100

    Advent of code December 6

---
+++ Dec6/Part1.cs
@@ -0,0 +1,41 @@
+﻿namespace Dec6;
+
+public class Part1
+{
+    public static void Calculate()
+    {
+        Console.WriteLine("Part1");
+
+        string message = File.ReadAllText("Input.txt");
+        int i = 0;
+        bool foundStartPacket = false;
+        while (i < message.Length && !foundStartPacket)
+        {
+            var tmp = message.Substring(i, 4);
+            foundStartPacket = StartOfPacketIdentified(tmp);
+            i++;
+        }
+
+        i--;
+        Console.WriteLine("Result:");
+        Console.WriteLine(i+4);
+
+    }
+
+    public static bool StartOfPacketIdentified(string s)
+    {
+        bool startOfPacketIdentified = true;
+        var charArray = s.ToCharArray();
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            for (int j = i + 1; j < charArray.Length; j++)
+            {
+                startOfPacketIdentified &= charArray[i] != charArray[j];
+            }
+        }
+
+        return startOfPacketIdentified;
+    }
+    
+    
+}
---
+++ Dec6/Part2.cs
@@ -0,0 +1,41 @@
+﻿namespace Dec6;
+
+public class Part2
+{
+    public static void Calculate()
+    {
+        Console.WriteLine("Part2");
+    
+        string message = File.ReadAllText("Input.txt");
+        int i = 0;
+        bool foundStartPacket = false;
+        while (i < message.Length && !foundStartPacket)
+        {
+            var tmp = message.Substring(i, 14);
+            foundStartPacket = StartOfPacketIdentified(tmp);
+            i++;
+        }
+        
+        Console.WriteLine("Result:");
+        Console.WriteLine(i+13);
+
+    }
+
+    public static bool StartOfPacketIdentified(string s)
+    {
+        bool startOfPacketIdentified = true;
+        var charArray = s.ToCharArray();
+
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            for (int j = i + 1; j < charArray.Length; j++)
+            {
+                startOfPacketIdentified &= charArray[i] != charArray[j];
+            }
+        }
+        
+        
+
+        return startOfPacketIdentified;
+    }
+}
