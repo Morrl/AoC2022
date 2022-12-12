dac0c3f6417d031dbef94cbebd55dc991bae6d81
Erik Lilja <fredriksson.erik@gmail.com>
2022-12-12 11:18:12 +0100

    Cleanup

--- Dec10/Part2.cs
+++ Dec10/Part2.cs
@@ -11,6 +11,7 @@ public class Part2
         var commands = File.ReadLines("Input.txt").ToList();
 
         var registryValueAtClockCycle = new List<int>();
+        //Add dummy value to shift all cycles 1 step
         registryValueAtClockCycle.Add(1);
         int currentValue = 1;
 
@@ -29,23 +30,7 @@ public class Part2
             registryValueAtClockCycle.Add(currentValue);
 
         }
-
-        // int cycle_20Val = registryValueAtClockCycle[20];
-        // int cycle_60Val = registryValueAtClockCycle[60];
-        // int cycle_100Val = registryValueAtClockCycle[100];
-        // int cycle_140Val = registryValueAtClockCycle[140];
-        // int cycle_180Val = registryValueAtClockCycle[180];
-        // int cycle_220Val = registryValueAtClockCycle[220];
-        //
-        // int signal20 = 20 * cycle_20Val;
-        // int signal60 = 60 * cycle_60Val;
-        // int signal100 = 100 * cycle_100Val;
-        // int signal140 = 140 * cycle_140Val;
-        // int signal180 = 180 * cycle_180Val;
-        // int signal200 = 220 * cycle_220Val;
-        //
-        //
-        // int result = signal20 + signal60 + signal100 + signal140 +signal180+ signal200;
+        
 
         StringBuilder sb = new StringBuilder();
 
